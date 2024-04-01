b7117d5f690bb6a96b7466bdf8fe2d3833df89c8
rammya-krishna <rammyakrishna018@gmail.com>
2024-04-01 14:53:05 +0530

    injected react component in UI

---
+++ app.js
@@ -0,0 +1,23 @@
+
+{/* <div id="firstparent">
+    <div id="secondparent">
+        <h1 id="first">first</h1>
+        <h1 id="second">secocnd</h1>
+    </div>
+</div> */}
+
+// REACT functional component
+const headings =React.createElement
+("div",
+{id:"firstparent"},
+    React.createElement
+    ("div",{id:"secondparent"},
+        [React.createElement
+        ("h1",{id:"first"},"first"),React.createElement
+        ("h1",{id:"second"},"second")])
+
+);
+console.log(headings) //object
+const root_=ReactDOM.createRoot(document.getElementById("root"));
+root_.render(headings)
+    
